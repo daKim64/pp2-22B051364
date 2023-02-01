4329256a3e92b8de53fbba611c07a512ecc18782
daKim64 <daniil.kim.64@gmail.com>
2023-02-02 04:15:12 +0600

    Uploaded TSIS2
    TSIS2

---
+++ tsis2/python_arrays.py
@@ -0,0 +1,20 @@
+# Arrays
+cars = ["Ford", "Volvo", "BMW"]
+
+# Access the Elements of an Array
+x = cars[0]
+cars[0] = "Toyota"
+
+# The Length of an Array
+x = len(cars)
+
+# Looping Array Elements
+for x in cars:
+  print(x)
+
+# Adding Array Elements
+cars.append("Honda")
+
+# Removing Array Elements
+cars.pop(0)
+cars.remove("Volvo")
